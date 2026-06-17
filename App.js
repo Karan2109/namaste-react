f226b0a694ce1ab7141f3dfd900838089f34d737
Karan Singh <karans21.dev@gmail.com>
2026-06-17 21:44:11 +0530

    Laying the foundation for react

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
-const header = React.createElement(
-    "div",
-    { id: "parent" },
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-    React.createElement(
-        "div",
-        { id: "child" },
-
-        React.createElement(
-            "h1",
-            { id: "heading1" },
-            "Hello I am Learning Namaste React"
-        ),
-
-        React.createElement(
-            "h2",
-            { id: "heading2" },
-            "Second Heading"
-        )
-    )
+// React Element
+const heading = React.createElement(
+    "h1",
+    { id: "heading" },
+    "Hello I am Learning Namaste React"
 );
+console.log(heading);
 
+// JSX = HTML like syntax
+const jsxHeading = (<h1 id="heading">Hello I am Learning JSX</h1>);
+console.log(jsxHeading);
+
+// React functional component
+const FirstComponent = () => {
+    return (
+        <div className="first-component">
+            <h1>Hello I am functional Component</h1>
+            <SecondComponent />
+        </div>
+    )
+}
+const name = "Karan Singh";
+// React functional component
+const SecondComponent = () => {
+    return (
+        <div className="second-component">
+            <h2>{name}</h2>
+            <h1>Hello I am second functional Component</h1>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(header);
+root.render(jsxHeading);
+root.render(<FirstComponent />);
