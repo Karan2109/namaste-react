cd870f664e7c49c1220d9acb1b375e81d6790558
Karan Singh <karans21.dev@gmail.com>
2026-06-24 08:38:16 +0530

    Fetched the Data from the swiggy API and removed the Mock data use

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,24 +1,38 @@
 import Resturantcard from "./Resturantcard";
-import { restaurants } from "../utils/mockData"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const Body = () => {
-    const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204&lng=73.8567&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        // Optional Chaining
+        const restaurants = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        setListOfRestaurants(restaurants)
+    }
+
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn"
                     onClick={() => {
-                        const filteredRestaurants = listOfRestaurants.filter((res) => res.rating > 4.5);
+                        const filteredRestaurants = listOfRestaurants.filter(
+                            (res) => res.info.avgRating > 4.5
+                        );
                         setListOfRestaurants(filteredRestaurants);
                         console.log(filteredRestaurants)
                     }}>Top Rated Restaurants</button>
             </div>
             <div className="res-container">
                 {
-                    listOfRestaurants.map((rest, id) => {
-                        return <Resturantcard key={id} resData={rest} />
+                    listOfRestaurants?.map((rest, id) => {
+                        return <Resturantcard key={rest.info.id} resData={rest.info} />
                     })
                 }
             </div>
--- src/components/Resturantcard.jsx
+++ src/components/Resturantcard.jsx
@@ -1,17 +1,31 @@
-const Resturantcard = (props) => {
-    const { resData } = props
+const Resturantcard = ({ resData }) => {
+    const {
+        name,
+        cuisines,
+        avgRating,
+        costForTwo,
+        cloudinaryImageId,
+        sla,
+    } = resData;
 
-    const { image, name, cuisine, rating, deliveryTime, costForTwo } = resData;
     return (
         <div className="res-card">
-            <img className="food" src={image} alt={name}></img>
-            <h3 className="name">{name}</h3>
-            <h4 className="cusine">Cusine: {cuisine}</h4>
-            <h4 className="rating">Rating: {rating}</h4>
-            <h4 className="time">Reaching time: {deliveryTime}</h4>
-            <h4 className="cost">Cost of product: {costForTwo}</h4>
+            <img
+                className="food"
+                src={
+                    "https://media-assets.swiggy.com/swiggy/image/upload/" +
+                    cloudinaryImageId
+                }
+                alt={name}
+            />
+
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>⭐ {avgRating}</h4>
+            <h4>{sla?.slaString}</h4>
+            <h4>{costForTwo}</h4>
         </div>
-    )
-}
+    );
+};
 
 export default Resturantcard;
